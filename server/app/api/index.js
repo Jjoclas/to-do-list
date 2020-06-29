0a28c6c0f2126c7a203be4ef1884755c01cea99e
Jose Claudio <jjoclas@hotmail.com>
2020-06-29 14:39:46 -0300

    adicionado texto adicional aos exemplos

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -4,14 +4,14 @@ var api = {}
 
 
 var Exemplos = [
-      { item : 'Estudar Calculo', classe : 'Educação', status : 150},
-      { item : 'Finalizar trabalho de Desenvolvimento web', classe : 'Educação', status : false},
-      { item : 'Pagar a fatura do cartão', classe : 'Finanças', status : true},
-      { item : 'Reunião as 17 horas', classe : 'Trabalho', status : false},
-      { item : 'Reunião de pais Sabado', classe : 'Filhos', status : false},
-      { item : 'Fazer convites para a festa de aniversario', classe : 'Filhos', status : false},
-      { item : 'Reservar um restaurante sexta a noite', classe : 'Relacionamento', status : true},
-      { item : 'Pelada as 20 horas', classe : 'Esporte', status : false}
+      { item : 'Estudar Calculo (EXEMPLO)', classe : 'Educação', status : 150},
+      { item : 'Finalizar trabalho de Desenvolvimento web  (EXEMPLO)', classe : 'Educação', status : false},
+      { item : 'Pagar a fatura do cartão (EXEMPLO)', classe : 'Finanças', status : true},
+      { item : 'Reunião as 17 horas (EXEMPLO)', classe : 'Trabalho', status : false},
+      { item : 'Reunião de pais Sabado (EXEMPLO)', classe : 'Filhos', status : false},
+      { item : 'Fazer convites para a festa de aniversario (EXEMPLO)', classe : 'Filhos', status : false},
+      { item : 'Reservar um restaurante sexta a noite (EXEMPLO)', classe : 'Relacionamento', status : true},
+      { item : 'Pelada as 20 horas (EXEMPLO)', classe : 'Esporte', status : false}
     ];
 
 var Tarefas = [];
